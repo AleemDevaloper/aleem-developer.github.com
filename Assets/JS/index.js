d22f04c8f1514cd85b274481183afcbd1f05902b
Muhammad Aleem <140176509+AleemDevaloper@users.noreply.github.com>
2024-01-02 00:17:02 +0500

    Add files via upload

---
+++ Assets/JS/index.js
@@ -0,0 +1,60 @@
+$('.icon').click(function () {
+    $('.click').css({"display":"block",});
+
+    $(this).click(function () {
+            $('.click').css({"display":"none",});
+            $(this).click(function () {
+                $('.click').css({"display":"block",});
+                $(this).click(function () {
+                    $('.click').css({"display":"none",});
+                    $(this).click(function () {
+                        $('.click').css({"display":"block",});
+                        $(this).click(function () {
+                            $('.click').css({"display":"none",});
+                            $(this).click(function () {
+                                $('.click').css({"display":"block",});
+                                $(this).click(function () {
+                                    $('.click').css({"display":"none",});
+                                    $(this).click(function () {
+                                        $('.click').css({"display":"block",});
+                                        $(this).click(function () {
+                                            $('.click').css({"display":"none",});
+                                            $(this).click(function () {
+                                                $('.click').css({"display":"block",});
+                                                $(this).click(function () {
+                                                    $('.click').css({"display":"none",});
+                                                    $(this).click(function () {
+                                                        $('.click').css({"display":"block",});
+                                                        $(this).click(function () {
+                                                            $('.click').css({"display":"none",});
+                                                            $(this).click(function () {
+                                                                $('.click').css({"display":"block",});
+                                                                $(this).click(function () {
+                                                                    $('.click').css({"display":"none",});
+                                                                    $(this).click(function () {
+                                                                        $('.click').css({"display":"block",});
+                                                                    
+                                                                })
+                                                            })
+                                                        })
+                                                    })
+                                                })
+                                            })
+                                        })
+                                    })
+                                })
+                            })
+                        })
+                    })
+                })
+            })
+        })
+    })
+})   
+
+
+
+
+       
+      
+    
