95fa52b812efc68cb6f7d49559496d2fe268f275
Muhammad Aleem <140176509+AleemDevaloper@users.noreply.github.com>
2024-02-09 21:14:27 +0500

    Add files via upload

--- Assets/JS/index.js
+++ Assets/JS/index.js
@@ -57,6 +57,65 @@ window.addEventListener("scroll" , function () {
 })
 
 
+$(document).ready(function(){
+    $('.c-slider').slick({
+        slidesToShow: 6,
+        slidesToScroll: 1,
+        arrows: true,
+        dots: false,
+        responsive: [
+            {
+                breakpoint: 2800,
+                settings: {
+                  slidesToShow: 4,
+                  slidesToScroll: 1,
+                  infinite: true,
+                  dots: false
+                }
+              },
+            {
+                breakpoint: 1500,
+                settings: {
+                  slidesToShow: 4,
+                  slidesToScroll: 1,
+                  infinite: true,
+                  dots: false
+                }
+              },
+            {
+              breakpoint: 1100,
+              settings: {
+                slidesToShow: 4,
+                slidesToScroll: 1,
+                infinite: true,
+                dots: false
+              }
+            },
+            {
+              breakpoint: 800,
+              settings: {
+                slidesToShow: 4,
+                slidesToScroll: 1,
+              }
+            },
+            {
+                breakpoint: 700,
+                settings: {
+                  slidesToShow: 3,
+                  slidesToScroll: 1,
+                }
+            },
+            {
+              breakpoint: 575,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+              }
+            }
+        ]
+    });
+});
+
 
        
       
